c40d9d7c7d23c8b05a8b1650aa5b560b84638599
Abdul Aziz Tetteh <abdulaziztetteh@gmail.com>
2024-02-18 19:14:29 +0000

    feat: Add student data access mapper

--- ssms-service/ssms-data-access/src/main/java/com/abduldevelops/ssms/api/dataaccess/student/mapper/StudentDataAccessMapper.java
+++ ssms-service/ssms-data-access/src/main/java/com/abduldevelops/ssms/api/dataaccess/student/mapper/StudentDataAccessMapper.java
@@ -9,12 +9,17 @@ import org.mapstruct.Mapping;
 @Mapper
 public interface StudentDataAccessMapper {
 
-    @Mapping(target="firstName",source = "studentName.firstName")
-    @Mapping(target="lastName",source = "studentName.lastName")
-    @Mapping(target="studentSlugID",source = "studentSlugID.value")
-    @Mapping(target="emailAddress",source = "emailAddress.emailAddress")
-    @Mapping(target="studentID",source = "id.value")
+    @Mapping(target = "firstName", source = "studentName.firstName")
+    @Mapping(target = "lastName", source = "studentName.lastName")
+    @Mapping(target = "studentSlugID", source = "studentSlugID.value")
+    @Mapping(target = "emailAddress", source = "emailAddress.emailAddress")
+    @Mapping(target = "studentID", source = "id.value")
     StudentEntity studentToStudentEntity(Student student);
 
-//    Student studentEntityToStudent(StudentEntity studentEntity);
+    @Mapping(target = "studentName.firstName", source = "firstName")
+    @Mapping(target = "studentName.lastName", source = "lastName")
+    @Mapping(target = "studentSlugID.value", source = "studentSlugID")
+    @Mapping(target = "emailAddress.emailAddress", source = "emailAddress")
+    @Mapping(target = "id.value", source = "studentID")
+    Student studentEntityToStudent(StudentEntity studentEntity);
 }
